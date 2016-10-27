5215ad4ada346fe276ccea3f725d7cdd85afdb4e
bexultankuandyk <bexultankuandyk@gmail.com>
2016-10-27 12:19:08 +0800

    Default AI fixed, added easy player in deployment controller

--- battleship/src/GameController.cs
+++ battleship/src/GameController.cs
@@ -84,8 +84,11 @@ public static class GameController
 		case AIOption.Hard:
 			_ai = new AIHardPlayer(_theGame);
 			break;
+		case AIOption.Easy:
+			_ai = new AIHardPlayer (_theGame);
+			break;
 		default:
-			_ai = new AIHardPlayer(_theGame);
+			_ai = new AIMediumPlayer(_theGame);
 			break;
 		}
 
